2caa7fe1450a94fbc567e9ce83672a1bf01f2985
rileygaffney3-NKLStC <rileygaffney3@icloud.com>
2023-08-14 18:22:46 -0300

    made changes to loop

--- Develop/script.js
+++ Develop/script.js
@@ -14,7 +14,7 @@ function writePassword() {
 }
 
 function generatePassword() {
-  var password = "word";
+  var password = "";
 
   // password character Sets
   var UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -24,6 +24,24 @@ function generatePassword() {
 
   // password criteria
   var passwordLength = prompt("select a password between 8 and 128 characters");
+  console.log(isNaN(passwordLength))
+  if(isNaN(passwordLength)) {
+    alert("Must be a number")
+    return;
+  
+  }
+
+
+
+  if (passwordLength <8) {
+    alert("Please pick a number greater than 8")
+    return;
+  }
+
+  if (passwordLength >128) {
+    alert("Please pick a number less than 128")
+    return;
+  }
   var useUppercase = confirm("Do you want uppercase characters?");
   var useLowercase = confirm("Do you want to use lowercase characters?");
   var useSymbols = confirm("Do you want to use symbols?");
@@ -62,11 +80,11 @@ function generatePassword() {
     validCharacters += NUMBERS;
   }
 
-  console.log(passwordLength);
-  console.log(useUppercase)
-  console.log(useLowercase);
-  console.log(useSymbols);
-  console.log(useNumbers);
+  //console.log(passwordLength);
+  // console.log(useUppercase)
+  // console.log(useLowercase);
+  // console.log(useSymbols);
+  // console.log(useNumbers);
   console.log(validCharacters);
 
   // ensure length is a number and not a string
@@ -74,7 +92,19 @@ function generatePassword() {
 
   // start; stop condition; pace;
   for (var i = 0; i < passwordLength; i++) {
-    console.log('Getting a new character');
+    // how to select a character at a position in a string (index)
+    console.log(i)
+
+
+  
+    // how to randamly get a decimal (float) between 0-1
+    // mulitply the float times the length of the string
+    // round down the decimal 
+
+    // select a character in string using index
+    // add the randomw character to the password variable 
+
+    
   }
 
   return password; 
